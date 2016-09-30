2499e1a0ce99b3e3c6a87382f6524f0f9565c462
Joshua Massover <joshua.massover@gmail.com>
2016-09-29 21:56:04 -0400

    Update model mixin and example

--- audio_recorder/models.py
+++ audio_recorder/models.py
@@ -1,12 +1,8 @@
-from django.core.files.storage import FileSystemStorage
 from django.db import models
-from django.conf import settings
 
 
 class AudioFileMixin(models.Model):
-    audio_file = models.FileField(
-        storage=FileSystemStorage(location=settings.MEDIA_ROOT)
-    )
+    audio_file = models.FileField()
 
     class Meta:
         abstract = True
--- test_project/audio_files/models.py
+++ test_project/audio_files/models.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.db import models
-from audio_recorder import AudioFileMixin
 
 
-class AudioFile(AudioFileMixin, models.Model):
-    pass
+class AudioFile(models.Model):
+    audio_file = models.FileField(
+        storage=FileSystemStorage(location=settings.MEDIA_ROOT)
+    )
